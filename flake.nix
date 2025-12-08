b0666bee336e84772ac5d371735f27f19b75e265
MoeLeak <i@leak.moe>
2025-12-08 17:13:33 +0800

    fix: If ICE failed, then switch to SDR **only**

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
         {
           default = pkgs.stdenv.mkDerivation rec {
             pname = "connecttool-qt";
-            version = "1.5.7";
+            version = "1.5.8";
 
             # Keep entire working tree (including untracked) so new sources are present.
             src = ./.;
